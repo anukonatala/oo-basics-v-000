f3f1f41205fc97ff9f3434e889d140aa1c12b396
Anusha Konatala <anu.konatala@gmail.com>
2018-06-12 00:51:28 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -47,4 +47,19 @@ class Shoe
     @condition = "new"
   end
  
+
 end
+
+#class Book
+#  attr_accessor :author, :page_count, :genre
+#  attr_reader :title
+
+#  def initialize(title)
+    #@title = title
+  #end
+
+  #def turn_page
+  #  puts "Flipping the page...wow, you read fast!"
+  #end
+
+#end
