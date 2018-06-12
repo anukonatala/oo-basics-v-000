c7dc6c3c26e79cff288ab3872089efea945e4ee6
Anusha Konatala <anu.konatala@gmail.com>
2018-06-12 00:53:41 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,65 +1,66 @@
 # Make your shoe class here!
 
-class Shoe
+#class Shoe
+#  
+#  def initialize(brand)
+#    @brand = brand
+#  end
+#  
+#  def brand
+#    @brand
+#  end
+#  
+#  def color=(color)
+#    @color = color
+#  end
+#  
+#  def color
+#    @color
+#  end
+##  
+#  def size=(size)
+#    @size = size
+#  end
+#  
+#  def size
+#    @size
+#  end
+#  
+#  def material=(material)
+#    @material = material
+#  end
+#  
+#  def material
+#    @material
+#  end
+#  
+#  def condition=(condition)
+#    @condition = condition
+#  end
+#  
+#  def condition
+#    @condition
+#  end
   
+#  def cobble
+#    puts "Your shoe is as good as new!"
+#    @condition = "new"
+#  end
+ 
+
+#end
+
+class Shoe
+  attr_accessor :material, :condition, :size, :color
+  attr_reader :brand
+
   def initialize(brand)
     @brand = brand
   end
-  
-  def brand
-    @brand
-  end
-  
-  def color=(color)
-    @color = color
-  end
-  
-  def color
-    @color
-  end
-  
-  def size=(size)
-    @size = size
-  end
-  
-  def size
-    @size
-  end
-  
-  def material=(material)
-    @material = material
-  end
-  
-  def material
-    @material
-  end
-  
-  def condition=(condition)
-    @condition = condition
-  end
-  
-  def condition
-    @condition
-  end
-  
+
   def cobble
     puts "Your shoe is as good as new!"
-    @condition = "new"
+    @condition = new
   end
- 
 
 end
-
-#class Book
-#  attr_accessor :author, :page_count, :genre
-#  attr_reader :title
-
-#  def initialize(title)
-    #@title = title
-  #end
-
-  #def turn_page
-  #  puts "Flipping the page...wow, you read fast!"
-  #end
-
-#end
